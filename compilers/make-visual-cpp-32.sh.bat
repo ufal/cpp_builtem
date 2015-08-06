1dec1105873051a926c98e48c0bab0c427f49979
Milan Straka <fox@ucw.cz>
2015-08-06 14:33:49 +0200

    Rename vc-12.0 to only vc.
    The compiler is kept in visual-cpp-version directory, so no version
    designation is needed. On the other hand, if it is present, the
    make-visual-cpp-{32,64} scripts must be version-specific and VS cannot
    be changed by only changing the visual-cpp symlink.

--- compilers/make-visual-cpp-32.sh.bat
+++ compilers/make-visual-cpp-32.sh.bat
@@ -7,7 +7,7 @@
 @rem License, v. 2.0. If a copy of the MPL was not distributed with this
 @rem file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-@set PATH=%~dp0visual-cpp-2013\vc-12.0\bin;%~dp0visual-cpp-2013\winsdk-v7.1a\bin;%PATH%
-@set INCLUDE=%~dp0visual-cpp-2013\vc-12.0\include;%~dp0visual-cpp-2013\winsdk-v7.1a\include
-@set LIB=%~dp0visual-cpp-2013\vc-12.0\lib;%~dp0visual-cpp-2013\winsdk-v7.1a\lib
+@set PATH=%~dp0visual-cpp-2013\vc\bin;%~dp0visual-cpp-2013\winsdk-v7.1a\bin;%PATH%
+@set INCLUDE=%~dp0visual-cpp-2013\vc\include;%~dp0visual-cpp-2013\winsdk-v7.1a\include
+@set LIB=%~dp0visual-cpp-2013\vc\lib;%~dp0visual-cpp-2013\winsdk-v7.1a\lib
 @%~dp0mingw32-make\mingw32-make.exe %*
