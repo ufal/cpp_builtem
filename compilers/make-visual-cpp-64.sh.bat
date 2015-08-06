db8e65bba71cb481e279ad8073b7fae149a0706f
Milan Straka <fox@ucw.cz>
2015-08-06 14:36:22 +0200

    Do not use visual-cpp-2013, but visual-cpp symlink.

--- compilers/make-visual-cpp-64.sh.bat
+++ compilers/make-visual-cpp-64.sh.bat
@@ -7,7 +7,7 @@
 @rem License, v. 2.0. If a copy of the MPL was not distributed with this
 @rem file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-@set PATH=%~dp0visual-cpp-2013\vc\bin\x86_amd64;%~dp0visual-cpp-2013\vc\bin;%~dp0visual-cpp-2013\winsdk-v7.1a\bin;%PATH%
-@set INCLUDE=%~dp0visual-cpp-2013\vc\include;%~dp0visual-cpp-2013\winsdk-v7.1a\include
-@set LIB=%~dp0visual-cpp-2013\vc\lib\amd64;%~dp0visual-cpp-2013\winsdk-v7.1a\lib\x64
+@set PATH=%~dp0visual-cpp\vc\bin\x86_amd64;%~dp0visual-cpp\vc\bin;%~dp0visual-cpp\winsdk-v7.1a\bin;%PATH%
+@set INCLUDE=%~dp0visual-cpp\vc\include;%~dp0visual-cpp\winsdk-v7.1a\include
+@set LIB=%~dp0visual-cpp\vc\lib\amd64;%~dp0visual-cpp\winsdk-v7.1a\lib\x64
 @%~dp0mingw32-make\mingw32-make.exe %*
