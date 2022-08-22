be1613e0b5f9e9984c1aac601b5de3392cf4e067
Milan Straka <milan@strakovi.com>
2022-08-23 00:32:07 +0200

    Update TDM-GCC to version 9.2.0.

---
+++ compilers/tdm-gcc-9.2.0-download/get.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This file is part of C++-Builtem <http://github.com/ufal/cpp_builtem/>.
+#
+# Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
+# Mathematics and Physics, Charles University in Prague, Czech Republic.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+wget https://github.com/jmeubank/tdm-gcc/releases/download/v9.2.0-tdm64-1/tdm64-gcc-9.2.0.exe
