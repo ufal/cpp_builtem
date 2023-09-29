df6e33bf3bcd7aa3063ef9f86d0c234ac9f75538
Milan Straka <milan@strakovi.com>
2023-09-29 18:59:35 +0200

    Update licence messages.

--- compilers/make-visual-cpp-32.sh.bat
+++ compilers/make-visual-cpp-32.sh.bat
@@ -1,7 +1,7 @@
 @rem This file is part of C++-Builtem <http://github.com/ufal/cpp_builtem/>.
 @rem
-@rem Copyright 2014 Institute of Formal and Applied Linguistics, Faculty of
-@rem Mathematics and Physics, Charles University in Prague, Czech Republic.
+@rem Copyright 2014-2023 Institute of Formal and Applied Linguistics, Faculty
+@rem of Mathematics and Physics, Charles University in Prague, Czech Republic.
 @rem
 @rem This Source Code Form is subject to the terms of the Mozilla Public
 @rem License, v. 2.0. If a copy of the MPL was not distributed with this
--- tests/compile_test.cpp
+++ tests/compile_test.cpp
@@ -1,3 +1,12 @@
+// This file is part of C++-Builtem <http://github.com/ufal/cpp_builtem/>.
+//
+// Copyright 2014-2023 Institute of Formal and Applied Linguistics, Faculty
+// of Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
